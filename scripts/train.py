ae1596b8e3d7051555eceaf399897d3273fef27a
Lochana Balivada <balivadalochana@gmail.com>
2026-01-23 17:33:47 +0530

    Test CI: worse model to verify deployment skip

--- scripts/train.py
+++ scripts/train.py
@@ -3,7 +3,7 @@ import joblib
 import os
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import LinearRegression()
+from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 
 # Load dataset
