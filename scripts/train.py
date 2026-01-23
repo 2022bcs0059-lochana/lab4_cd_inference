0a22cc9935e42fff0c5161694ab37528261cca2c
Lochana Balivada <balivadalochana@gmail.com>
2026-01-23 17:27:28 +0530

    Lab 4 automated CI/CD inference - triggering workflow

--- scripts/train.py
+++ scripts/train.py
@@ -15,12 +15,13 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-#model
+# Model
 model = RandomForestRegressor(n_estimators=100, max_depth=15, random_state=42)
 model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)
 
+# Evaluation
 mse = mean_squared_error(y_test, y_pred)
 r2 = r2_score(y_test, y_pred)
 
