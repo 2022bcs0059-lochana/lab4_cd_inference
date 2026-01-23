21467213bae206eb1fe3e8ba384c5737836f8742
Lochana Balivada <balivadalochana@gmail.com>
2026-01-23 18:04:22 +0530

    Worse model - 2022BCS0059

--- scripts/train.py
+++ scripts/train.py
@@ -3,7 +3,7 @@ import joblib
 import os
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 
 # Load dataset
@@ -16,7 +16,7 @@ X_train, X_test, y_train, y_test = train_test_split(
 )
 
 # Model
-model = RandomForestRegressor(n_estimators=100, max_depth=15, random_state=42)
+model = LinearRegression()
 model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)
